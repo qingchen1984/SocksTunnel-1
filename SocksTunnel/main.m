fdf9f14f07bb296b2e51046f91516e5a26f63f10
liaogang <ynwlgh@icloud.com>
2019-11-27 16:36:33 +0800

    update

--- SocksTunnel/main.m
+++ SocksTunnel/main.m
@@ -14,52 +14,90 @@
 
 void printUsage()
 {
-    printf("socksTunnel host port username password local_bind_port \n");
+    printf("参数选项说明: r 远程地址:端口, u和p 远程socks用户名及密码[可不传此项],  l 本地本址:端口\n");
+    printf("例子: socksTunnel -r 3.3.3.3:2020 -l 127.0.0.1:8888 -u username -p password\n");
 }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        printUsage();
         
-        if (argc == 6) {
-
-            const char *host_ = argv[1];
-            const char *port_ = argv[2];
+        NSString *host;
+        NSString *port;
+        NSString *username;
+        NSString *password;
+        NSString *localInterface;
+        NSString *localPort;
+        
+        
+        while( 1 )
+        {
+            int option = getopt(argc, (char**)argv, "r:l:u:p:h");
+            if (option == -1) {
+                break;
+            }
             
-            const char *username_ = argv[3];
-            const char *password_ = argv[4];
+            switch (option) {
+                case 'r':
+                {
+                    printf("remote_server: %s\n", optarg);
+                    NSString *tmp = [NSString stringWithUTF8String:optarg];
+                    NSArray *a = [tmp componentsSeparatedByString:@":"];
+                    host = a.firstObject;
+                    port = a.lastObject;
+                    break;
+                }
+                case 'l':
+                {
+                    printf("local_bind: %s\n", optarg);
+                    NSString *tmp = [NSString stringWithUTF8String:optarg];
+                    NSArray *a = [tmp componentsSeparatedByString:@":"];
+                    localInterface = a.firstObject;
+                    localPort = a.lastObject;
+                    break;
+                }
+                case 'u':
+                {
+                    printf("username: %s\n", optarg);
+                    NSString *tmp = [NSString stringWithUTF8String:optarg];
+                    username = tmp;
+                    break;
+                }
+                case 'p':
+                {
+                    printf("pasword: %s\n", optarg);
+                    NSString *tmp = [NSString stringWithUTF8String:optarg];
+                    password = tmp;
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
             
-            const char *local_ = argv[5];
-            
-            NSString *host = [NSString stringWithUTF8String:host_];
-            NSString *port = [NSString stringWithUTF8String:port_];
-            NSString *username = [NSString stringWithUTF8String:username_];
-            NSString *password = [NSString stringWithUTF8String:password_];
-            NSString *local = [NSString stringWithUTF8String:local_];
-            
-            
-            
-            SOCKS5Proxy *forwardServer;
-            forwardServer = [[SOCKS5Proxy alloc] init];
-            [forwardServer setOutgoingHost:host port:port.intValue];
+        }
+        
+        
+        SOCKS5Proxy *forwardServer;
+        forwardServer = [[SOCKS5Proxy alloc] init];
+        [forwardServer setOutgoingHost:host port:port.intValue];
+        if (username && password) {
             [forwardServer setOutgoingSocksUsername:username password:password];
-            
-            
-            if([forwardServer startProxyOnPort:local.intValue])
-            {
-                printf("forward sever working on local port %d ..\n", local.intValue);
-                CFRunLoopRun();
-            }
-            else{
-                printf("bind failed at port %d \n", local.intValue);
-            }
-
+        }
+        
+        uint16_t uport = localPort.intValue;
+        
+        if([forwardServer startProxyOnPort: uport])
+        {
+            printf("forward sever working on local port %d ..\n", uport);
+            CFRunLoopRun();
         }
         else{
-            printUsage();
+            printf("bind failed at port %d \n", uport);
         }
         
+        return 0;
     }
-    return 0;
 }
-
