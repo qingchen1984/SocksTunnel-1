548a83ed6b565d5b0fa92aeddd4514f55709a1bd
liaogang <ynwlgh@icloud.com>
2021-07-09 14:35:25 +0800

    修改选项

--- SocksTunnel/main.m
+++ SocksTunnel/main.m
@@ -15,30 +15,26 @@
 
 void printUsage()
 {
-    printf("socks 转发管道服务,用以连接本地和远程socks5\n");
-    printf("参数选项说明: r 远程地址:端口, u和p 远程socks用户名及密码[可不传此项],  l 本地本址:端口\n");
-    printf("例子: socksTunnel -r 3.3.3.3:2020 -l 127.0.0.1:8888 -u username -p password\n");
-    printf("---\n");
+    printf("Socks5 转发管道服务,用以连接本地和远程socks5\n");
+    printf("SocksTunnel -r remote_host:remote_port [-l localport] [-a username:password]\n");
 }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        [DDLog addLogger:[DDOSLogger sharedInstance]];
-        
-        printUsage();
+        //[DDLog addLogger:[DDOSLogger sharedInstance]];
         
         NSString *host;
         NSString *port;
         NSString *username;
         NSString *password;
+        
         NSString *localInterface;
         NSString *localPort;
         
-        
         while( 1 )
         {
-            int option = getopt(argc, (char**)argv, "r:l:u:p:h");
+            int option = getopt(argc, (char**)argv, "r:l:a:");
             if (option == -1) {
                 break;
             }
@@ -58,58 +54,74 @@ int main(int argc, const char * argv[]) {
                     printf("local_bind: %s\n", optarg);
                     NSString *tmp = [NSString stringWithUTF8String:optarg];
                     NSArray *a = [tmp componentsSeparatedByString:@":"];
-                    localInterface = a.firstObject;
-                    localPort = a.lastObject;
+                    if (a.count > 0) {
+                        localInterface = a.firstObject;
+                        localPort = a.lastObject;
+                    }else{
+                        localPort = tmp;
+                    }
+                    
                     break;
                 }
-                case 'u':
+                case 'a':
                 {
-                    printf("username: %s\n", optarg);
+                    printf("username password: %s\n", optarg);
                     NSString *tmp = [NSString stringWithUTF8String:optarg];
-                    username = tmp;
-                    break;
-                }
-                case 'p':
-                {
-                    printf("pasword: %s\n", optarg);
-                    NSString *tmp = [NSString stringWithUTF8String:optarg];
-                    password = tmp;
+                    NSArray *a = [tmp componentsSeparatedByString:@":"];
+                    username = a.firstObject;
+                    password = a.lastObject;
                     break;
                 }
                 default:
                 {
-                    break;
+                    printUsage();
+                    return 0;
                 }
             }
+        }
+        
+        if (host.length > 0 && port.length > 0) {
             
-        }
-        
-        
-        SOCKS5Proxy *forwardServer;
-        forwardServer = [[SOCKS5Proxy alloc] init];
-        [forwardServer setOutgoingHost:host port:port.intValue];
-        if (username.length > 0 && password.length > 0) {
-            [forwardServer setOutgoingSocksUsername:username password:password];
-        }
-        
-        uint16_t uport = localPort.intValue;
-        
-        if([forwardServer startProxyOnPort: uport])
-        {
-            printf("forward sever working on local port %d ..\n", uport);
-            CFRunLoopRun();
-        }
-        else{
-            printf("bind failed at port %d.\n", uport);
-            char *p = "sadf";
-            if (getchar() == 0) {
-                p = 0;
+
+            SOCKS5Proxy *forwardServer;
+            forwardServer = [[SOCKS5Proxy alloc] init];
+            [forwardServer setOutgoingHost:host port:port.intValue];
+            
+            if (username.length > 0 && password.length > 0) {
+                [forwardServer setOutgoingSocksUsername:username password:password];
             }
             
-            char *cc = p;
-            printf("%s, %s.", cc, p);
+            uint16_t uport;
+            if(localPort)
+            {
+                uport = localPort.intValue;
+            }else{
+                uport = random() % 1000 + 5000;
+            }
+
+            printf("\n");
+            
+            if([forwardServer startProxyOnPort: uport])
+            {
+                printf("forward sever working on local port %d ..\n", uport);
+                CFRunLoopRun();
+            }
+            else{
+                printf("bind failed at port %d.\n", uport);
+                char *p = "sadf";
+                if (getchar() == 0) {
+                    p = 0;
+                }
+                
+                char *cc = p;
+                printf("%s, %s.", cc, p);
+            }
+
         }
-        
+        else{
+            printUsage();
+        }
+
         return 0;
     }
 }
